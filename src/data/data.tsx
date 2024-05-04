7cfa09c40c755989aa23cfcdab82268b3b9eb57f
Riwan William Bououli <riwan.b@live.fr>
2024-05-04 11:27:18 +0200

    updating the portfolio project section

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -12,10 +12,12 @@ import profilepic from '../../public/my-picture.png';
 import GithubIcon from '../components/Icon/GithubIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -79,7 +81,8 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/CV_2024-04-12_BOUOULI_Riwan_William.pdf',
+      // doesnt work, i would like users to be able to dl my cv
+      href: './CV_2024-04-12_BOUOULI_Riwan_William.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -190,54 +193,54 @@ export const skills: SkillGroup[] = [
  * Portfolio section
  */
 export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
+  // {
+  //   title: 'Cloning Vinted website',
+  //   description: 'A fully operationnal Vinted clone',
+  //   url: 'https://clone-of-vinted.netlify.app/',
+  //   image: porfolioImage1,
+  // },
   {
     title: 'Project title 2',
     description: 'Give a short description of your project here.',
     url: 'https://reactresume.com',
     image: porfolioImage2,
   },
-  // {
-  //   title: 'Project title 3',
-  //   description: 'Give a short description of your project here.',
-  //   url: 'https://reactresume.com',
-  //   image: porfolioImage3,
-  // },
+  {
+    title: 'Project title 3',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage3,
+  },
   {
     title: 'Project title 4',
     description: 'Give a short description of your project here.',
     url: 'https://reactresume.com',
     image: porfolioImage4,
   },
-  // {
-  //   title: 'Project title 5',
-  //   description: 'Give a short description of your project here.',
-  //   url: 'https://reactresume.com',
-  //   image: porfolioImage5,
-  // },
-  // {
-  //   title: 'Project title 6',
-  //   description: 'Give a short description of your project here.',
-  //   url: 'https://reactresume.com',
-  //   image: porfolioImage6,
-  // },
-  // {
-  //   title: 'Project title 7',
-  //   description: 'Give a short description of your project here.',
-  //   url: 'https://reactresume.com',
-  //   image: porfolioImage7,
-  // },
   {
-    title: 'Project title 8',
+    title: 'Project title 5',
     description: 'Give a short description of your project here.',
     url: 'https://reactresume.com',
-    image: porfolioImage8,
+    image: porfolioImage5,
   },
+  {
+    title: 'Project title 6',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage6,
+  },
+  {
+    title: 'Project title 7',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage7,
+  },
+  // {
+  //   title: 'Project title 8',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage8,
+  // },
   // {
   //   title: 'Project title 9',
   //   description: 'Give a short description of your project here.',
--- src/data/portfolioItems.tsx
+++ src/data/portfolioItems.tsx
@@ -1,11 +1,9 @@
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
 import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
 import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import {PortfolioItem} from './dataDef';
 
 /**
@@ -13,54 +11,54 @@ import {PortfolioItem} from './dataDef';
  */
 
 export const portfolioItems: PortfolioItem[] = [
+  // {
+  //   title: 'Cloning Vinted website',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage1,
+  // },
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Clone of Vinted',
+    description: 'A fully operationnal Vinted clone. Back-end using Node.js and Front-end using React: Home page of offers, Login, Sign-in, Post offers and Buying page with Stripes ',
+    url: 'https://clone-of-vinted.netlify.app/',
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Pokedex Pokemon',
+    description: 'Using the biggest API about pokemon. You can learn everything about them, class them by types, pokedex number, learn about their level, abilities, evolutions ... ',
+    url: 'https://pokemon-riwibou.netlify.app/',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Clone of TripAdvisor',
+    description: 'Cloning the front-end of that site only using CSS to explore the endless possibilities of this technology',
+    url: 'https://clone-of-tripadvisor.netlify.app/',
     image: porfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Clone of Deliveroo',
+    description: 'Cloning the front-end and coding the logic for the cart with React and Vanilla JS',
+    url: 'https://clone-of-deliveroo.netlify.app/',
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Palace Factory',
+    description: 'Creating a website for an event venue and party hall for a client',
+    url: 'https://palace-factory-v1.netlify.app/',
     image: porfolioImage6,
   },
   {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'My Portfolio',
+    description: 'Using React, Next, TypeScript',
+    url: 'https://riwan-william-bououli.netlify.app/',
     image: porfolioImage7,
   },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
+  // {
+  //   title: 'Project title 8',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage8,
+  // },
   // {
   //   title: 'Project title 9',
   //   description: 'Give a short description of your project here.',
