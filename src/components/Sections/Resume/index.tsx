e0f060128f06fde10f4e884b98cd3c58bc71d8be
Riwan William Bououli <riwan.b@live.fr>
2024-05-14 12:35:14 +0200

    added incons for technologies and created icon components

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,9 +1,12 @@
 import {FC, memo} from 'react';
 
-import {education, experience, SectionId, skills} from '../../../data/data';
+import {education, experience, SectionId} from '../../../data/data';
+import JsIcon from '../../Icon/JsIcon';
+import NextjsIcon from '../../Icon/NextjsIcon';
+import NodeIcon from '../../Icon/NodeIcon';
+import ReactIcon from '../../Icon/ReactIcon';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
-import {SkillGroup} from './Skills';
 import TimelineItem from './TimelineItem';
 
 const Resume: FC = memo(() => {
@@ -20,12 +23,13 @@ const Resume: FC = memo(() => {
             <TimelineItem item={item} key={`${item.title}-${index}`} />
           ))}
         </ResumeSection>
-        <ResumeSection title="Skills">
+        <ResumeSection title="Technologies">
           <p className="pb-8"></p>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {skills.map((skillgroup, index) => (
-              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
-            ))}
+            <JsIcon />
+            <NextjsIcon />
+            <ReactIcon />
+            <NodeIcon />
           </div>
         </ResumeSection>
       </div>
